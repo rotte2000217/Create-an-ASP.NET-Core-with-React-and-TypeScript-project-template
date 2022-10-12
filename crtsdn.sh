0b0486a842f032f97b7ea3a382ef31f6fbc1a515
Muhd Raihan <muhdraihan1140@gmail.com>
2022-10-12 23:16:46 +0800

    added functionality to catch errors

--- crtsdn.sh
+++ crtsdn.sh
@@ -3,11 +3,23 @@ if [ "$#" -ne 1 ]; then
     exit 2
 fi
 
+# colors
+CYAN='\033[1;36m'
+NC='\033[0m' # No Color
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+RED='\033[0;31m'
+
 # set project name variable
 projName=$1
 
 # create a new dotnet project with react template
-dotnet new react -o $projName
+{
+dotnet new react -o $projName 
+} || { 
+printf "${RED}\nAn error occurred while creating dotnet project${NC}\n"
+exit 2
+}
 cd $projName
 
 # copy important files into temporary folder
@@ -20,8 +32,17 @@ cp ClientApp/src/setupProxy.js ClientApp/src/serviceWorkerRegistration.js Client
 
 # remove ClientApp, create react app with tyescript, rename clientapp
 rm -rf ClientApp
-npx create-react-app clientapp --template typescript
-mv clientapp ClientApp
+
+{
+    npx create-react-app clientapp --template typescript
+} || { 
+   printf "${RED}\nAn error occurred while creating react app.${NC}\n\n"
+   rm -rf ../$tmpCopy
+   cd ..
+   rm -rf $projName
+   exit 2
+} 
+mv clientapp ClientApp 
 
 # copy back the files from tmpCopy and remove tmpCopy
 cp ../$tmpCopy/tmpSrc/* ClientApp/src
@@ -33,11 +54,6 @@ rm -rf ../$tmpCopy
 sed -i '$s/}/,\n"prestart": "node aspnetcore-https && node aspnetcore-react"}/' ClientApp/package.json
 
 # success output
-CYAN='\033[1;36m'
-NC='\033[0m' # No Color
-YELLOW='\033[1;33m'
-GREEN='\033[1;32m'
-
 printf "\n\n************************************************************************\n\n"
 printf "${GREEN}Success! You have created an ASP.Net Core app with React TypeScript!\n\n${NC}"
 printf "Usage:\n"
